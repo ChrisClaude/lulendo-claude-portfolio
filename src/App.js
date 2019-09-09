c916facd076b77c568fa7c39d9317f8293c7bce4
ChClaude <christ.tchambila@gmail.com>
2019-09-09 10:43:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            {/*NavBar*/}
+            <nav className="white">
+                <div className="nav-wrapper container">
+                    <a href="#" className="brand-logo custom-black-text">Lulendo</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><a href="#" className="custom-black-text">Home</a></li>
+                        <li><a href="#" className="custom-black-text">My creations</a></li>
+                        <li><a href="#" className="custom-black-text">About me</a></li>
+                        <li><a href="#" className="custom-black-text">Get in touch</a></li>
+                    </ul>
+                </div>
+            </nav>
+        {/*Preloader*/}
+            <section className="container">
+                <div className="preloader-wrapper big active">
+                    <div className="spinner-layer spinner-blue-only">
+                        <div className="circle-clipper left">
+                            <div className="circle"/>
+                        </div>
+                        <div className="gap-patch">
+                            <div className="circle"/>
+                        </div>
+                        <div className="circle-clipper right">
+                            <div className="circle"/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
 }
 
 export default App;
