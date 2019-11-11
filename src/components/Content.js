1d8e52bfbd1be9d70e8c3ec529d69f2aa52cae65
ChClaude <christ.tchambila@gmail.com>
2019-11-11 20:24:21 +0200

    content links added

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,40 +9,38 @@ class Content extends Component {
                 <div className="image-box">
                     <img src={claude_pic} alt="Claude Christ's pic" className="round-pic"/>
                 </div>
-                <div>
-                    <h3>Claude De-Tchambila</h3>
-                    <h4>Software Developer</h4>
-                    <div>
-                        <FontAwesomeIcon icon={['fab', 'linkedin']}/>
-                        <FontAwesomeIcon icon={['fab', 'github-square']}/>
-                        <FontAwesomeIcon icon={['fab', 'twitter-square']}/>
-                        <FontAwesomeIcon icon={['fab', 'facebook-square']}/>
+                <div className="resume">
+                    <h3 className="text-center">Claude De-Tchambila</h3>
+                    <h4 className="text-center">Software Developer</h4>
+                    <div className="social-icons-box">
+                        <a href="https://www.linkedin.com/in/claude-christ-de-tchambila-a720ba143/"><FontAwesomeIcon icon={['fab', 'linkedin']} className="social-icons"/></a>
+                        <a href="https://github.com/ChClaude"><FontAwesomeIcon icon={['fab', 'github-square']} className="social-icons"/></a>
+                        <a href="https://twitter.com/ClaudeChrist_DT"><FontAwesomeIcon icon={['fab', 'twitter-square']} className="social-icons"/></a>
                     </div>
                     <p>I am a software developer that specializes in Python/Django. I also work on .NET Core with C# to
-                        mainly
-                        develop web and mobile apps.</p>
+                        mainly develop web and mobile apps.</p>
                     <div className="skills-box">
-                        <h3>SKILLS & PROJECTS</h3>
-                        <div className="skill-set">
-                            <div className="skill-language">
+                        <h3 className="text-center">SKILLS & PROJECTS</h3>
+                        <div className="skills-set">
+                            <div className="skills-lang text-center">
                                 <FontAwesomeIcon icon={['fab', 'python']} className="ic-lang"/>
                                 <h4>Python</h4>
                             </div>
-                            <div className="skill-language">
+                            <div className="skills-lang text-center">
                                 <FontAwesomeIcon icon={['fab', 'js-square']} className="ic-lang"/>
                                 <h4>JavaScript</h4>
                             </div>
-                            <div className="skill-language">
+                            <div className="skills-lang text-center">
                                 <FontAwesomeIcon icon={['fab', 'js-square']} className="ic-lang"/>
                                 <h4>C#</h4>
                             </div>
-                            <div className="skill-language">
+                            <div className="skills-lang text-center">
                                 <FontAwesomeIcon icon={['fab', 'java']} className="ic-lang"/>
                                 <h4>Java</h4>
                             </div>
                         </div>
                         <a href="https://drive.google.com/uc?authuser=0&id=1MrHNZjn2zPfwtQuJji8FfhdxW0-p3qpv&export=download"
-                           className="btn btn-primary" >
+                           className="btn btn-primary default-ver-margin" >
                             Download Resume
                         </a>
                     </div>
